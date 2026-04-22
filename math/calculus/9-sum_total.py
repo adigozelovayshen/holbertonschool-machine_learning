df96ca49a16c653a83674861b95ced637129476f
adigozelovayshen <13597@holbertonstudents.com>
2026-04-22 18:22:24 +0000

    Clean version of task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -3,9 +3,8 @@
 
 
 def summation_i_squared(n):
-    """Calculates the sum of i^2 from 1 to n using Faulhaber's formula"""
+    """Calculates the sum of i^2 from 1 to n"""
     if not isinstance(n, int) or n < 1:
         return None
-    
     result = (n * (n + 1) * (2 * n + 1)) // 6
     return result
