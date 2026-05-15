edea20f5205129c22e3e8bd8317f418136142001
adigozelovayshen <13759@holbertonstudents.com>
2026-05-15 20:28:09 +0000

    Add pdf method to Normal class

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -28,3 +28,17 @@ class Normal:
     def x_value(self, z):
         """Verilmiş z-score üçün x dəyərini hesablayır"""
         return (z * self.stddev) + self.mean
+
+    def pdf(self, x):
+        """Verilmiş x dəyəri üçün PDF dəyərini hesablayır"""
+        pi = 3.1415926536
+        e = 2.7182818285
+
+        # Düsturun hissələri:
+        # 1 / (stddev * sqrt(2 * pi))
+        coefficient = 1 / (self.stddev * ((2 * pi) ** 0.5))
+
+        # -0.5 * ((x - mean) / stddev) ** 2
+        exponent = -0.5 * (((x - self.mean) / self.stddev) ** 2)
+
+        return coefficient * (e ** exponent)
