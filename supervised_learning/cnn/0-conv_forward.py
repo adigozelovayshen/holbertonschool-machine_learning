60d99fa162887fda9caef16887b453e3af4ee934
adigozelovayshen <adigozlovaysn@gmail.com>
2026-07-24 17:46:42 +0000

    Add convolutional forward propagation

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
-"""
-Convolutional Forward Propagation Module
-"""
+"""Convolutional Forward Propagation"""
+
 import numpy as np
 
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
-    Performs forward propagation over a convolutional layer
+    Performs forward propagation over a convolutional layer.
 
-    Parameters:
-    - A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
-    - W: numpy.ndarray of shape (kh, kw, c_prev, c_new)
-    - b: numpy.ndarray of shape (1, 1, 1, c_new)
-    - activation: activation function to apply
-    - padding: "same" or "valid"
-    - stride: tuple of (sh, sw)
+    Args:
+        A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+        W: numpy.ndarray of shape (kh, kw, c_prev, c_new)
+        b: numpy.ndarray of shape (1, 1, 1, c_new)
+        activation: activation function
+        padding: "same" or "valid"
+        stride: tuple (sh, sw)
 
     Returns:
-    - Output of the convolutional layer
+        The output of the convolutional layer
     """
+
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, _, c_new = W.shape
     sh, sw = stride
@@ -27,37 +27,42 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     if padding == "same":
         ph = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
         pw = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
-    elif padding == "valid":
-        ph, pw = 0, 0
+    else:
+        ph = 0
+        pw = 0
 
-    h_out = int((h_prev + 2 * ph - kh) / sh) + 1
-    w_out = int((w_prev + 2 * pw - kw) / sw) + 1
-
-    padded_A = np.pad(
+    A_prev_pad = np.pad(
         A_prev,
         ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode='constant',
-        constant_values=0
+        mode="constant"
     )
 
-    Z = np.zeros((m, h_out, w_out, c_new))
+    h_new = int((h_prev + 2 * ph - kh) / sh) + 1
+    w_new = int((w_prev + 2 * pw - kw) / sw) + 1
 
-    for i in range(h_out):
-        for j in range(w_out):
-            v_start = i * sh
-            v_end = v_start + kh
-            h_start = j * sw
-            h_end = h_start + kw
+    Z = np.zeros((m, h_new, w_new, c_new))
 
-            slice_A = padded_A[:, v_start:v_end, h_start:h_end, :]
+    for i in range(h_new):
+        for j in range(w_new):
+            vert_start = i * sh
+            vert_end = vert_start + kh
+            horiz_start = j * sw
+            horiz_end = horiz_start + kw
 
-            for k in range(c_new):
-                Z[:, i, j, k] = np.sum(
-                    slice_A * W[:, :, :, k],
-                    axis=(1, 2, 3)
+            current_slice = A_prev_pad[
+                :,
+                vert_start:vert_end,
+                horiz_start:horiz_end,
+                :
+            ]
+
+            for c in range(c_new):
+                Z[:, i, j, c] = (
+                    np.sum(
+                        current_slice * W[:, :, :, c],
+                        axis=(1, 2, 3)
+                    )
+                    + b[0, 0, 0, c]
                 )
 
-    Z = Z + b
-    A = activation(Z)
-
-    return A
+    return activation(Z)
