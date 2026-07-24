Fix w_out calculation in conv_forward

supervised_learning/cnn/0-conv_forward.py
@@ -8,6 +8,17 @@ import numpy as np
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
     Performs forward propagation over a convolutional layer
+
+    Parameters:
+    - A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+    - W: numpy.ndarray of shape (kh, kw, c_prev, c_new)
+    - b: numpy.ndarray of shape (1, 1, 1, c_new)
+    - activation: activation function to apply
+    - padding: "same" or "valid"
+    - stride: tuple of (sh, sw)
+
+    Returns:
+    - Output of the convolutional layer
     """
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, _, c_new = W.shape
@@ -20,12 +31,13 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         ph, pw = 0, 0
 
     h_out = int((h_prev + 2 * ph - kh) / sh) + 1
-    w_out = int((w_prev + 2 * pw - kh) / sw) + 1
+    w_out = int((w_prev + 2 * pw - kw) / sw) + 1
 
     padded_A = np.pad(
         A_prev,
         ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode="constant"
+        mode='constant',
+        constant_values=0
     )
 
     Z = np.zeros((m, h_out, w_out, c_new))