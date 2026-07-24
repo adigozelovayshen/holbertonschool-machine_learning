a92cb42eb252e7ed989a0aa50396f34cb4c5270d
adigozelovayshen <adigozlovaysn@gmail.com>
2026-07-24 17:31:22 +0000

    Fix conv_forward and add README files

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -8,17 +8,6 @@ import numpy as np
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
     Performs forward propagation over a convolutional layer
-
-    Parameters:
-    - A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
-    - W: numpy.ndarray of shape (kh, kw, c_prev, c_new)
-    - b: numpy.ndarray of shape (1, 1, 1, c_new)
-    - activation: activation function to apply
-    - padding: "same" or "valid"
-    - stride: tuple of (sh, sw)
-
-    Returns:
-    - Output of the convolutional layer
     """
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, _, c_new = W.shape
@@ -31,25 +20,24 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         ph, pw = 0, 0
 
     h_out = int((h_prev + 2 * ph - kh) / sh) + 1
-    w_out = int((w_prev + 2 * pw - kw) / sw) + 1
+    w_out = int((w_prev + 2 * pw - kh) / sw) + 1
 
     padded_A = np.pad(
         A_prev,
         ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode='constant',
-        constant_values=0
+        mode="constant"
     )
 
     Z = np.zeros((m, h_out, w_out, c_new))
 
     for i in range(h_out):
         for j in range(w_out):
-            vert_start = i * sh
-            vert_end = vert_start + kh
-            horiz_start = j * sw
-            horiz_end = horiz_start + kw
+            v_start = i * sh
+            v_end = v_start + kh
+            h_start = j * sw
+            h_end = h_start + kw
 
-            slice_A = padded_A[:, vert_start:vert_end, horiz_start:horiz_end, :]
+            slice_A = padded_A[:, v_start:v_end, h_start:h_end, :]
 
             for k in range(c_new):
                 Z[:, i, j, k] = np.sum(
--- supervised_learning/cnn/README.md
+++ supervised_learning/cnn/README.md
@@ -1,4 +1,1 @@
-This directory contains projects and implementations for Convolutional Neural Networks (CNN) as part of the machine learning curriculum.
-
-## Tasks
-* 0-conv_forward.py: Performs forward propagation over a convolutional layer of a neural network.
+# Conv Forward
