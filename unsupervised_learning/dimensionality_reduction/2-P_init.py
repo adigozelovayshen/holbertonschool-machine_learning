1d2e6a8d1677ab38e3eece1dd870aa1d7cdca86d
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-12 19:52:01 +0000

    Fix pycodestyle E501 line length issues in 2-P_init.py

--- unsupervised_learning/dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/dimensionality_reduction/2-P_init.py
@@ -7,23 +7,23 @@ import numpy as np
 
 def P_init(X, perplexity):
     """
-    Initializes all variables required to calculate the P affinities in t-SNE.
+    Initializes variables required to calculate P affinities in t-SNE.
 
     Parameters:
     - X (numpy.ndarray): Dataset of shape (n, d)
     - perplexity (float): Perplexity for Gaussian distributions
 
     Returns:
-    - D (numpy.ndarray): Squared pairwise distance matrix of shape (n, n)
-    - P (numpy.ndarray): P affinities matrix initialized to zeros of shape (n, n)
-    - betas (numpy.ndarray): Beta values initialized to ones of shape (n, 1)
-    - H (float): Shannon entropy for the perplexity with base 2
+    - D (numpy.ndarray): Squared pairwise distance matrix (n, n)
+    - P (numpy.ndarray): P affinities matrix (n, n)
+    - betas (numpy.ndarray): Beta values (n, 1)
+    - H (float): Shannon entropy for perplexity with base 2
     """
     n, d = X.shape
 
-    # Kvadratik Evklid məsafəsi: ||x_i - x_j||^2 = ||x_i||^2 + ||x_j||^2 - 2*(x_i . x_j)
     sum_X = np.sum(np.square(X), axis=1)
-    D = sum_X.reshape(-1, 1) + sum_X.reshape(1, -1) - 2 * np.matmul(X, X.T)
+    D = sum_X.reshape(-1, 1) + sum_X.reshape(1, -1)
+    D = D - 2 * np.matmul(X, X.T)
     np.fill_diagonal(D, 0)
 
     P = np.zeros((n, n))
