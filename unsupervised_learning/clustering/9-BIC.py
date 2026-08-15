83e1fb776595c3a71f9080756c173ae172392019
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-15 13:54:22 +0000

    Fix pycodestyle formatting and kmax/kmin validation in 9-BIC.py

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-"""Module that finds the best number of clusters for a GMM using BIC."""
+"""
+Module that finds the best number of clusters
+for a GMM using the Bayesian Information Criterion.
+"""
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
-def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """Finds the best number of clusters for a GMM using BIC."""
+def BIC(X, kmin=1, kmax=None, iterations=1000,
+        tol=1e-5, verbose=False):
+    """
+    Finds the best number of clusters for a GMM using BIC.
+    """
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None, None, None
     if not isinstance(kmin, int) or kmin <= 0:
