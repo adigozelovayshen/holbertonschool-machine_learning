fd4f8dc7e90fca980bc534c3e59c43667caaa347
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-15 12:39:34 +0000

    Implement Task 9: BIC for GMM

---
+++ unsupervised_learning/clustering/9-BIC.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Module that finds the best number of clusters for a GMM using BIC."""
+import numpy as np
+expectation_maximization = __import__('8-EM').expectation_maximization
+
+
+def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
+    """
+    Finds the best number of clusters for a GMM using BIC.
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None, None
+    n, d = X.shape
+    if kmax is None:
+        kmax = n
+
+    results = []
+    log_likelihoods = []
+    bics = []
+    ks = range(kmin, kmax + 1)
+
+    for k in ks:
+        pi, m, S, g, ll = expectation_maximization(
+            X, k, iterations, tol, verbose
+        )
+        if pi is None:
+            return None, None, None, None
+
+        # Number of parameters:
+        # pi: k - 1
+        # m: k * d
+        # S: k * d * (d + 1) / 2
+        p = (k - 1) + (k * d) + (k * d * (d + 1) / 2)
+        bic = p * np.log(n) - 2 * ll
+
+        results.append((pi, m, S))
+        log_likelihoods.append(ll)
+        bics.append(bic)
+
+    best_k = ks[np.argmin(bics)]
+    best_result = results[np.argmin(bics)]
+
+    return best_k, best_result, np.array(log_likelihoods), np.array(bics)
