47d7cf50e3a930702c91a6add0ab577e11951bcb
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-15 12:38:44 +0000

    Fix E741 and E501 style errors in 8-EM.py

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -7,15 +7,7 @@ maximization = __import__('7-maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
-    """
-    Performs expectation maximization for a GMM.
-    - X: numpy.ndarray of shape (n, d)
-    - k: positive integer (number of clusters)
-    - iterations: positive integer (max iterations)
-    - tol: non-negative float (tolerance for early stopping)
-    - verbose: boolean (whether to print log likelihood information)
-    Returns: pi, m, S, g, l, or None on failure
-    """
+    """Performs expectation maximization for a GMM."""
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
     if not isinstance(k, int) or k <= 0:
@@ -28,40 +20,29 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None, None
 
     pi, m, S = initialize(X, k)
-    if pi is None or m is None or S is None:
-        return None, None, None, None, None
-
-    g, prev_l = expectation(X, pi, m, S)
-    if g is None or prev_l is None:
-        return None, None, None, None, None
+    g, prev_ll = expectation(X, pi, m, S)
 
     if verbose:
-        print("Log Likelihood after 0 iterations: {:.5f}".format(prev_l))
+        print("Log Likelihood after 0 iterations: {:.5f}".format(prev_ll))
 
-    best_pi, best_m, best_S, best_g, best_l = pi, m, S, g, prev_l
+    best_pi, best_m, best_S, best_g, best_ll = pi, m, S, g, prev_ll
     final_iter = 0
 
     for i in range(1, iterations + 1):
         pi, m, S = maximization(X, g)
-        if pi is None or m is None or S is None:
-            return None, None, None, None, None
-
-        g, l = expectation(X, pi, m, S)
-        if g is None or l is None:
-            return None, None, None, None, None
-
-        best_pi, best_m, best_S, best_g, best_l = pi, m, S, g, l
+        g, ll = expectation(X, pi, m, S)
+        best_pi, best_m, best_S, best_g, best_ll = pi, m, S, g, ll
         final_iter = i
 
         if verbose and i % 10 == 0:
-            print("Log Likelihood after {} iterations: {:.5f}".format(i, l))
+            print("Log Likelihood after {} iterations: {:.5f}".format(i, ll))
 
-        if abs(l - prev_l) <= tol:
+        if abs(ll - prev_ll) <= tol:
             break
-
-        prev_l = l
+        prev_ll = ll
 
     if verbose and final_iter % 10 != 0:
-        print("Log Likelihood after {} iterations: {:.5f}".format(final_iter, best_l))
+        print("Log Likelihood after {} iterations: {:.5f}".format(
+            final_iter, best_ll))
 
-    return best_pi, best_m, best_S, best_g, best_l
+    return best_pi, best_m, best_S, best_g, best_ll
