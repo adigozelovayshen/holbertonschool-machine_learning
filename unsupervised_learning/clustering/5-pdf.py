27fc259214a446b65704a282563c4ac736303073
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-15 12:33:03 +0000

    Fix pycodestyle line length error in 5-pdf.py

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -17,7 +17,8 @@ def pdf(X, m, S):
         return None
     if not isinstance(S, np.ndarray) or len(S.shape) != 2:
         return None
-    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0] or S.shape[0] != S.shape[1]:
+    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0] or \
+       S.shape[0] != S.shape[1]:
         return None
 
     n, d = X.shape
