de381147ba75b87b99a65bc16ea0daa3a25406e0
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-15 13:36:15 +0000

    Final version of 9-BIC.py for Holberton

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -6,46 +6,46 @@ expectation_maximization = __import__('8-EM').expectation_maximization
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """Finds the best number of clusters for a GMM using BIC."""
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None, None, None
     if not isinstance(kmin, int) or kmin <= 0:
         return None, None, None, None
     if kmax is not None and (not isinstance(kmax, int) or kmax <= 0):
         return None, None, None, None
-    
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None, None, None
+    if not isinstance(tol, (int, float)) or tol < 0:
+        return None, None, None, None
+    if not isinstance(verbose, bool):
+        return None, None, None, None
+
     n, d = X.shape
     if kmax is None:
         kmax = n
     if kmin > kmax or kmin > n:
         return None, None, None, None
 
-    results, log_likelihoods, bics = [], [], []
+    results, l_list, b_list = [], [], []
     ks = range(kmin, kmax + 1)
-    
+
     for k in ks:
-        pi, m, S, g, ll = expectation_maximization(
-            X, k, iterations, tol, verbose
-        )
-        
+        pi, m, S, g, log_l = expectation_maximization(
+            X, k, iterations, tol, verbose)
+
         if pi is None:
-            # Uğursuz olduqda sonsuzluq əlavə edirik ki, 
-            # np.argmin bunu seçməsin, amma proses qırılmasın
-            log_likelihoods.append(-np.inf)
-            bics.append(np.inf)
-            results.append((None, None, None))
-        else:
-            p = (k - 1) + (k * d) + (k * d * (d + 1) / 2)
-            bic = p * np.log(n) - 2 * ll
-            log_likelihoods.append(ll)
-            bics.append(bic)
-            results.append((pi, m, S))
+            return None, None, None, None
 
-    log_likelihoods = np.array(log_likelihoods)
-    bics = np.array(bics)
-    
-    # Əgər hamısı None olubsa (heç bir k işləməyibsə)
-    if np.all(bics == np.inf):
-        return None, None, None, None
+        p = (k - 1) + (k * d) + (k * d * (d + 1) / 2)
+        bic = p * np.log(n) - 2 * log_l
 
-    best_idx = np.argmin(bics)
-    return ks[best_idx], results[best_idx], log_likelihoods, bics
+        l_list.append(log_l)
+        b_list.append(bic)
+        results.append((pi, m, S))
+
+    l = np.array(l_list)
+    b = np.array(b_list)
+    best_index = np.argmin(b)
+    best_k = ks[best_index]
+    best_result = results[best_index]
+
+    return best_k, best_result, l, b
