Fix BIC calculation and parameters checking

unsupervised_learning/clustering/9-BIC.py
@@ -39,32 +39,26 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     if not isinstance(verbose, bool):
         return None, None, None, None
 
-    l = []
-    b = []
+    l = np.zeros(kmax - kmin + 1)
+    b = np.zeros(kmax - kmin + 1)
     results = []
 
     for k in range(kmin, kmax + 1):
-        try:
-            res = expectation_maximization(
-                X, k, iterations, tol, verbose
-            )
-            pi, m, S, g, log_l = res
-            if pi is None or m is None or S is None or log_l is None:
-                return None, None, None, None
-        except Exception:
+        pi, m, S, g, log_l = expectation_maximization(
+            X, k, iterations, tol, verbose
+        )
+        if pi is None or m is None or S is None or log_l is None:
             return None, None, None, None
 
-        l.append(log_l)
+        idx = k - kmin
+        l[idx] = log_l
         results.append((pi, m, S))
 
         # p = number of parameters
+        # (k - 1) priors + (k * d) means + k * d * (d + 1) / 2 covariances
         p = (k - 1) + (k * d) + (k * d * (d + 1) / 2)
         # BIC = p * ln(n) - 2 * l
-        bic_val = p * np.log(n) - 2 * log_l
-        b.append(bic_val)
-
-    l = np.array(l)
-    b = np.array(b)
+        b[idx] = p * np.log(n) - 2 * log_l
 
     best_idx = np.argmin(b)
     best_k = kmin + best_idx