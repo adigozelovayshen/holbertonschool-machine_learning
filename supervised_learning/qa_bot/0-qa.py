e2d4443462d451045b48afaf36728f231bcb85fb
adigozelovayshen <adigozelovayshen@gmail.com>
2026-08-22 22:04:43 +0000

    Fix pycodestyle line length error in 0-qa.py

--- supervised_learning/qa_bot/0-qa.py
+++ supervised_learning/qa_bot/0-qa.py
@@ -15,42 +15,34 @@ def question_answer(question, reference):
 
     Returns:
         str: The answer snippet, or None if no answer is found
-    ```"""
-    # Tokenizer-in yüklənməsi
+    """
     tokenizer = BertTokenizer.from_pretrained(
         'bert-large-uncased-whole-word-masking-finetuned-squad'
     )
-    
-    # Modelin TensorFlow Hub-dan yüklənməsi
-    model = hub.load('[https://tfhub.dev/see--/bert-uncased-tf2-qa/1](https://tfhub.dev/see--/bert-uncased-tf2-qa/1)')
+    model = hub.load('https://tfhub.dev/see--/bert-uncased-tf2-qa/1')
 
-    # Suallar və istinad mətni üçün tokenlərin yaradılması
     question_tokens = tokenizer.tokenize(question)
     reference_tokens = tokenizer.tokenize(reference)
 
-    # BERT formatına uyğun input tokenlərin birləşdirilməsi: [CLS] + question + [SEP] + reference + [SEP]
-    tokens = ['[CLS]'] + question_tokens + ['[SEP]'] + reference_tokens + ['[SEP]']
+    tokens = (['[CLS]'] + question_tokens + ['[SEP]'] +
+              reference_tokens + ['[SEP]'])
     input_ids = tokenizer.convert_tokens_to_ids(tokens)
     input_mask = [1] * len(input_ids)
-    segment_ids = [0] * (len(question_tokens) + 2) + [1] * (len(reference_tokens) + 1)
+    segment_ids = ([0] * (len(question_tokens) + 2) +
+                   [1] * (len(reference_tokens) + 1))
 
-    # Tensor formatına keçirilməsi
     input_ids = tf.constant([input_ids], dtype=tf.int32)
     input_mask = tf.constant([input_mask], dtype=tf.int32)
     segment_ids = tf.constant([segment_ids], dtype=tf.int32)
 
-    # Modelin çağırılması
     outputs = model([input_ids, input_mask, segment_ids])
-    
-    # Cavabın başlanğıc və bitiş indekslərinin təyini
+
     start_index = tf.argmax(outputs[0][0][1:-1]) + 1
     end_index = tf.argmax(outputs[1][0][1:-1]) + 1
 
-    # Əgər uyğun cavab tapılmadısa
     if start_index > end_index:
         return None
 
-    # Tokenləri cavab mətni kimi birləşdirmək
     answer_tokens = tokens[start_index: end_index + 1]
     answer = tokenizer.convert_tokens_to_string(answer_tokens)
 
