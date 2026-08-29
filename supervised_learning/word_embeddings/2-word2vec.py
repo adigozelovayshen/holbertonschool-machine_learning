d057b3912b44f413767ecec9b2e92d865cd80b4b
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-29 13:02:01 +0000

    Fix E127 indentation error in word2vec_model signature

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -6,7 +6,7 @@ import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
-                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
+                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
     Creates, builds and trains a gensim word2vec model.
 
