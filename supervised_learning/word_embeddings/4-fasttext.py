c66ff51e8765691bfe0ec153091ce9ac22a0dc35
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-29 19:39:53 +0000

    Fix W292 pycodestyle error

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,43 +1,28 @@
 #!/usr/bin/env python3
-"""
-Creates and trains a gensim fastText model
+"""creates and trains a gensim fastText model
 """
 import gensim
 
 
-def fasttext_model(sentences, vector_size=100, min_count=5, window=5,
-                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
-    """
-    Creates, builds and trains a gensim fastText model
-    """
+def fasttext_model(sentences, vector_size=100, min_count=5,
+                   window=5, negative=5, cbow=True,
+                   epochs=5, seed=0, workers=1):
+    """Creates, builds and trains a gensim fastText model"""
     sg = 0 if cbow else 1
 
-    try:
-        model = gensim.models.FastText(
-            size=vector_size,
-            min_count=min_count,
-            window=window,
-            negative=negative,
-            sg=sg,
-            seed=seed,
-            workers=workers
-        )
-    except TypeError:
-        model = gensim.models.FastText(
-            vector_size=vector_size,
-            min_count=min_count,
-            window=window,
-            negative=negative,
-            sg=sg,
-            seed=seed,
-            workers=workers
-        )
-
-    model.build_vocab(sentences)
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
-        epochs=epochs
+    model = gensim.models.FastText(
+        sentences=sentences,
+        vector_size=vector_size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=sg,
+        epochs=epochs,
+        seed=seed,
+        workers=workers
     )
 
+    model.build_vocab(sentences)
+    model.train(sentences, total_examples=model.corpus_count,
+                epochs=model.epochs)
     return model
