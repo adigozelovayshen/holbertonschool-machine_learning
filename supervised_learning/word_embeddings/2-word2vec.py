6426ea236a752f902b8a1c80c81f7bcffaed41fd
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-29 19:12:55 +0000

    Fix Word2Vec model double-training and parameter names

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Word2Vec model module
+Creates and trains a gensim word2vec model
 """
 import gensim
 
@@ -8,7 +8,7 @@ import gensim
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
-    Creates, builds and trains a gensim word2vec model.
+    Creates, builds and trains a gensim word2vec model
     """
     sg = 0 if cbow else 1
 
