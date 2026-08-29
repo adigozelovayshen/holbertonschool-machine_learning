6eb417fc589e2fba86685a7e241875e7d2aa06b2
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-29 13:17:53 +0000

    Swap window and negative order to match FastText structure

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -9,20 +9,6 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
     Creates, builds and trains a gensim word2vec model.
-
-    Args:
-        sentences: list of sentences to be trained on
-        vector_size: dimensionality of the embedding layer
-        min_count: minimum number of occurrences of a word
-        window: maximum distance between current and predicted word
-        negative: size of negative sampling
-        cbow: boolean to determine training type
-        epochs: number of iterations to train over
-        seed: seed for random number generator
-        workers: number of worker threads to train the model
-
-    Returns:
-        the trained Word2Vec model
     """
     sg = 0 if cbow else 1
 
