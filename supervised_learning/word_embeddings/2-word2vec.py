8c00512fbabd312cb864c473b5722d41fb67faeb
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-29 18:49:41 +0000

    Fix Word2Vec output to match checker vector seed values

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -12,29 +12,16 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     """
     sg = 0 if cbow else 1
 
-    try:
-        model = gensim.models.Word2Vec(
-            sentences=sentences,
-            size=vector_size,
-            min_count=min_count,
-            window=window,
-            negative=negative,
-            sg=sg,
-            iter=epochs,
-            seed=seed,
-            workers=workers
-        )
-    except TypeError:
-        model = gensim.models.Word2Vec(
-            sentences=sentences,
-            vector_size=vector_size,
-            min_count=min_count,
-            window=window,
-            negative=negative,
-            sg=sg,
-            epochs=epochs,
-            seed=seed,
-            workers=workers
-        )
+    model = gensim.models.Word2Vec(
+        sentences=sentences,
+        vector_size=vector_size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=sg,
+        epochs=epochs,
+        seed=seed,
+        workers=workers
+    )
 
     return model
