1bc61edfc60b8bc0dce56fb5445ff8441110fb9d
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-29 10:55:40 +0000

    Fix import and train steps for Word2Vec

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-"""Word2Vec model module"""
-from gensim.models import Word2Vec
+"""
+Word2Vec model module
+"""
+import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
@@ -14,7 +16,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         min_count: minimum number of occurrences of a word for use in training
         window: maximum distance between current and predicted word
         negative: size of negative sampling
-        cbow: boolean to determine training type (True for CBOW, False for Skip-gram)
+        cbow: boolean to determine training type
         epochs: number of iterations to train over
         seed: seed for random number generator
         workers: number of worker threads to train the model
@@ -23,15 +25,22 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         the trained Word2Vec model
     """
     sg = 0 if cbow else 1
-    model = Word2Vec(
-        sentences=sentences,
+
+    model = gensim.models.Word2Vec(
         vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
         sg=sg,
-        epochs=epochs,
         seed=seed,
         workers=workers
     )
+
+    model.build_vocab(sentences)
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=epochs
+    )
+
     return model
