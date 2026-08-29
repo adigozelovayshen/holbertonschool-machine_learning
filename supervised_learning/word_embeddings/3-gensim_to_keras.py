d3fbb498690c353e30d2d23e5d4b22a79375badf
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-29 13:24:22 +0000

    Add Task 3 gensim_to_keras implementation

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -7,13 +7,13 @@ import tensorflow as tf
 
 def gensim_to_keras(model):
     """
-    Converts a gensim word2vec model to a trainable keras Embedding layer.
+    Converts a trained gensim word2vec model to a trainable keras Embedding layer.
 
     Args:
         model: trained gensim word2vec model
 
     Returns:
-        the trainable keras Embedding layer
+        trainable keras Embedding layer
     """
     weights = model.wv.vectors
     layer = tf.keras.layers.Embedding(
