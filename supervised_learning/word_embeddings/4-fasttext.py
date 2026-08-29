c9576b44f6dfa257e3c19b8fa6c3d838660807c8
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-29 19:34:01 +0000

    Fix W292 no newline at end of file

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
 """
-FastText model module
+Creates and trains a gensim fastText model
 """
 import gensim
 
 
-def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
-                   window=5, cbow=True, epochs=5, seed=0, workers=1):
+def fasttext_model(sentences, vector_size=100, min_count=5, window=5,
+                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
-    Creates, builds and trains a gensim fastText model.
+    Creates, builds and trains a gensim fastText model
     """
     sg = 0 if cbow else 1
 
-    model = gensim.models.FastText(
-        vector_size=vector_size,
-        min_count=min_count,
-        negative=negative,
-        window=window,
-        sg=sg,
-        seed=seed,
-        workers=workers
-    )
+    try:
+        model = gensim.models.FastText(
+            size=vector_size,
+            min_count=min_count,
+            window=window,
+            negative=negative,
+            sg=sg,
+            seed=seed,
+            workers=workers
+        )
+    except TypeError:
+        model = gensim.models.FastText(
+            vector_size=vector_size,
+            min_count=min_count,
+            window=window,
+            negative=negative,
+            sg=sg,
+            seed=seed,
+            workers=workers
+        )
+
     model.build_vocab(sentences)
     model.train(
         sentences,
