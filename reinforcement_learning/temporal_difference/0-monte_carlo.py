e7d7f34da3d59c9613fa33c4f79bb2db4ac2b552
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-30 21:24:38 +0000

    Fix Monte Carlo indexing and trajectory collection

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -9,23 +9,12 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                 alpha=0.1, gamma=0.99):
     """
     Performs the Monte Carlo algorithm for value estimation.
-
-    Parameters:
-    - env: environment instance
-    - V: numpy.ndarray of shape (s,) containing value estimate
-    - policy: function taking a state and returning next action
-    - episodes: total number of episodes to train over
-    - max_steps: maximum number of steps per episode
-    - alpha: learning rate
-    - gamma: discount rate
-
-    Returns:
-    - V: updated value estimate
     """
     for episode in range(episodes):
         res = env.reset()
         state = res[0] if isinstance(res, tuple) else res
-        episode_data = []
+        states = []
+        rewards = []
 
         for step in range(max_steps):
             action = policy(state)
@@ -36,7 +25,8 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                 next_state, reward, done, info = step_res
                 truncated = False
 
-            episode_data.append((state, reward))
+            states.append(state)
+            rewards.append(reward)
 
             if done or truncated:
                 break
@@ -44,13 +34,12 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
             state = next_state
 
         G = 0
-        episode_states = [x[0] for x in episode_data]
-
-        for i in range(len(episode_data) - 1, -1, -1):
-            s, r = episode_data[i]
+        for i in range(len(states) - 1, -1, -1):
+            s = states[i]
+            r = rewards[i]
             G = gamma * G + r
 
-            if s not in episode_states[:i]:
+            if s not in states[:i]:
                 V[s] = V[s] + alpha * (G - V[s])
 
     return V
