dcdbb2c762a631600013dbfce3c96dc98f0b06ab
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-30 11:07:06 +0000

    Fix generator exhaustion in train_new_from_iterator

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,50 +1,57 @@
 #!/usr/bin/env python3
-"""Defines the Dataset class that loads and preps a translation dataset"""
-from transformers import AutoTokenizer
+"""
+Dataset class for Machine Translation
+"""
+import transformers
 from setup import load_pt2en
 
 
 class Dataset:
-    """Loads and preps a dataset for machine translation"""
+    """
+    Loads and pre-processes a dataset for machine translation
+    """
 
     def __init__(self):
-        """Class constructor"""
+        """
+        Class constructor
+        """
         self.data_train = load_pt2en('train')
         self.data_valid = load_pt2en('validation')
+
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train)
+            self.data_train
+        )
 
     def tokenize_dataset(self, data):
-        """Creates sub-word tokenizers for our dataset
+        """
+        Creates sub-word tokenizers for the dataset
 
         Args:
-            data: tf.data.Dataset whose examples are formatted as a tuple
-                (pt, en)
-                pt is the tf.Tensor containing the Portuguese sentence
-                en is the tf.Tensor containing the corresponding English
-                sentence
+            data: tf.data.Dataset whose examples are formatted as (pt, en)
 
         Returns:
-            tokenizer_pt, tokenizer_en
+            tokenizer_pt: Portuguese tokenizer
+            tokenizer_en: English tokenizer
         """
-        base_tokenizer_pt = AutoTokenizer.from_pretrained(
-            'neuralmind/bert-base-portuguese-cased')
-        base_tokenizer_en = AutoTokenizer.from_pretrained(
-            'bert-base-uncased')
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased',
+            use_fast=True
+        )
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+            'bert-base-uncased',
+            use_fast=True
+        )
 
-        def pt_sentences():
-            """Generator yielding Portuguese sentences as strings"""
-            for pt, _ in data.as_numpy_iterator():
-                yield pt.decode('utf-8')
+        pt_list = [pt.numpy().decode('utf-8') for pt, _ in data]
+        en_list = [en.numpy().decode('utf-8') for _, en in data]
 
-        def en_sentences():
-            """Generator yielding English sentences as strings"""
-            for _, en in data.as_numpy_iterator():
-                yield en.decode('utf-8')
-
-        tokenizer_pt = base_tokenizer_pt.train_new_from_iterator(
-            pt_sentences(), vocab_size=2 ** 13)
-        tokenizer_en = base_tokenizer_en.train_new_from_iterator(
-            en_sentences(), vocab_size=2 ** 13)
+        tokenizer_pt = tokenizer_pt.train_new_from_iterator(
+            pt_list,
+            vocab_size=2**13
+        )
+        tokenizer_en = tokenizer_en.train_new_from_iterator(
+            en_list,
+            vocab_size=2**13
+        )
 
         return tokenizer_pt, tokenizer_en
