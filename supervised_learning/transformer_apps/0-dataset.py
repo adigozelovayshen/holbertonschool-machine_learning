c8a0fc98a304a9fa1dbdae844d6e0f7c499eb14e
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-30 10:42:58 +0000

    Fix trailing newline in 0-dataset.py

---
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+Dataset class for Machine Translation
+"""
+import transformers
+from setup import load_pt2en
+
+
+class Dataset:
+    """
+    Loads and pre-processes a dataset for machine translation
+    """
+
+    def __init__(self):
+        """
+        Class constructor
+        """
+        self.data_train = load_pt2en('train')
+        self.data_valid = load_pt2en('validation')
+
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train
+        )
+
+    def tokenize_dataset(self, data):
+        """
+        Creates sub-word tokenizers for the dataset
+
+        Args:
+            data: tf.data.Dataset whose examples are formatted as (pt, en)
+
+        Returns:
+            tokenizer_pt: Portuguese tokenizer
+            tokenizer_en: English tokenizer
+        """
+        pt_pretrained = 'neuralmind/bert-base-portuguese-cased'
+        en_pretrained = 'bert-base-uncased'
+
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
+            pt_pretrained,
+            use_fast=True
+        )
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+            en_pretrained,
+            use_fast=True
+        )
+
+        def pt_generator():
+            for pt, _ in data:
+                yield pt.numpy().decode('utf-8')
+
+        def en_generator():
+            for _, en in data:
+                yield en.numpy().decode('utf-8')
+
+        tokenizer_pt = tokenizer_pt.train_new_from_iterator(
+            pt_generator(),
+            vocab_size=2**13
+        )
+        tokenizer_en = tokenizer_en.train_new_from_iterator(
+            en_generator(),
+            vocab_size=2**13
+        )
+
+        return tokenizer_pt, tokenizer_en
