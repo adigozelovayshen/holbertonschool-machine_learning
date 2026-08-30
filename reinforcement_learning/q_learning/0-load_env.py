5705215f7476c6a716f17f7d28e99112de7cb558
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-30 20:36:04 +0000

    Fix PEP8 no newline at end of file error

---
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module to load the FrozenLake environment from Gymnasium
+"""
+import gymnasium as gym
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """
+    Loads the pre-made FrozenLakeEnv environment from gymnasium.
+
+    Parameters:
+    - desc: list of lists containing a custom map description, or None
+    - map_name: string containing pre-made map name, or None
+    - is_slippery: boolean indicating if ice is slippery
+
+    Returns:
+    - env: the initialized gymnasium environment
+    """
+    env = gym.make(
+        'FrozenLake-v1',
+        desc=desc,
+        map_name=map_name,
+        is_slippery=is_slippery
+    )
+    return env
