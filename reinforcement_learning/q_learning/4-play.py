44de692c8a4ca67b841ec8d3228935ce3330c39e
adigozelovayshen <adigozlovaysn@gmail.com>
2026-08-30 20:51:01 +0000

    Fix W292 no newline at end of file in 4-play.py

---
+++ reinforcement_learning/q_learning/4-play.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Module to play an episode using a trained Q-table
+"""
+import numpy as np
+
+
+def play(env, Q, max_steps=100):
+    """
+    Has the trained agent play an episode.
+
+    Parameters:
+    - env: FrozenLakeEnv instance
+    - Q: numpy.ndarray containing the Q-table
+    - max_steps: maximum number of steps in the episode
+
+    Returns:
+    - total_rewards: total rewards earned during the episode
+    - rendered_outputs: list of rendered outputs representing board
+    """
+    reset_res = env.reset()
+    state = reset_res[0] if isinstance(reset_res, tuple) else reset_res
+
+    rendered_outputs = [env.render()]
+    total_rewards = 0
+
+    for step in range(max_steps):
+        action = np.argmax(Q[state])
+
+        step_res = env.step(action)
+        if len(step_res) == 5:
+            next_state, reward, done, truncated, info = step_res
+        else:
+            next_state, reward, done, info = step_res
+            truncated = False
+
+        rendered_outputs.append(env.render())
+        total_rewards += reward
+        state = next_state
+
+        if done or truncated:
+            break
+
+    return total_rewards, rendered_outputs
